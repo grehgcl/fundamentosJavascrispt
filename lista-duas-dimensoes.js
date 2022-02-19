8ec3ead0770df3c217254e2d68fe5f5aff1e7249
digregorio <grehgcl@hotmail.com>
2022-02-19 10:28:43 -0300

    continuando aula

---
+++ lista-duas-dimensoes.js
@@ -0,0 +1,8 @@
+const alunos = ['Joao', 'Juliana', 'Caio', 'Ana']
+
+const mediaDosAlunos =[10, 7, 9 , 6]
+
+let listaDenotasEAlunos = [alunos, mediaDosAlunos]
+
+console.log(`${listaDenotasEAlunos [0][0]}, sua media é
+${listaDenotasEAlunos [1] [0]} `)
